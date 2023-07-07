c8935912ac1e8368c863cc0b978656fa4d1bad0a
Martín Frola <86237783+martinfrola@users.noreply.github.com>
2023-07-07 16:41:48 +0000

    Impacto en db de CN

--- src/API/tickets.js
+++ src/API/tickets.js
@@ -29,5 +29,44 @@ export const updateTickets = (ticket) => {
       },
       body: JSON.stringify(ticket),
     }
-  );
+  ).then(() => saveTicketCN(ticket));
+};
+
+export const saveTicketCN = async (data) => {
+  const url = "https://api-cn.cngrupo.com.ar/api/insertaTicket";
+  const newData = {
+    lat: data.lat ? data.lat.toString() : "",
+    lng: data.lng ? data.lng.toString() : "",
+    inicio: data.inicio || "",
+    fin: data.fin || "",
+    estadoInicial: data.estadoInicial
+      ? parseFloat(data.estadoInicial).toString()
+      : "",
+    entregaBruta: data.entregaBruta
+      ? parseFloat(data.entregaBruta).toString()
+      : "",
+    estadoFinal: data.estadoFinal
+      ? parseFloat(data.estadoFinal).toString()
+      : "",
+    numeroVenta: data.numeroVenta || "",
+    cliente: data.cliente || "",
+    producto: data.producto || "",
+    patente: data.patente || "",
+    patenteAuxilio: data.patenteAuxilio || "",
+    choferLegajo: data.choferLegajo ? parseInt(data.choferLegajo) : null,
+    maquina: data.maquina || "",
+    horometro: data.horometro || "",
+    operacion: data.operacion || "",
+    bloqueID: data.bloqueID || "",
+    supervisorLegajo: data.supervisorLegajo || "",
+    cuarteo: data.cuarteo || "",
+  };
+  const formattedData = JSON.stringify(newData)
+    .split("\n")
+    .filter(Boolean)
+    .map((line) => JSON.parse(line));
+
+  const finalData = JSON.stringify(formattedData, null, 2);
+
+  await axios.post(url, JSON.parse(finalData));
 };
